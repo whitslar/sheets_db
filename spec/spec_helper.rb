54350c90f726fb663c7377c91bb2225f66a7f32c
Ravi Gadad <ravi@ablschools.com>
2016-10-03 17:37:51 -0700

    Basic infrastructure additions

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,2 +1,11 @@
+require 'simplecov'
+SimpleCov.start
+
 $LOAD_PATH.unshift File.expand_path('../../lib', __FILE__)
 require 'sheets_db'
+
+RSpec.configure do |config|
+  config.run_all_when_everything_filtered = true
+  config.expose_dsl_globally = false
+  config.order = 'random'
+end
